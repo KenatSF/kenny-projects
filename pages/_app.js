ae198cca2fe452936a2c2bd0d01d3393eeb12f84
KenatSF <banerabh@gmail.com>
2022-10-13 00:12:58 -0500

    First big change

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
+import MainLayout from '../src/layouts/main';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ src/layouts/main/index.js
@@ -0,0 +1,67 @@
+import { Box, Flex, HStack, IconButton, useDisclosure, useColorModeValue, Image, Heading, Link } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+
+import Footer from "./footer";
+
+
+
+const MainLayout = ({ children }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    return (
+        <Flex minH="100vh" direction="column">
+            <Box
+                mx="auto"
+                maxW={"7xl"}
+                width="100%"
+                bg={useColorModeValue("white", "gray.800")}
+                px={4}
+            >
+                <Flex
+                    bg={useColorModeValue("white", "gray.800")}
+                    color={useColorModeValue("gray.600", "white")}
+                    minH={"60px"}
+                    py={{ base: 2 }}
+                    px={{ base: 4 }}
+                    borderBottom={1}
+                    borderStyle={"solid"}
+                    borderColor={useColorModeValue("gray.200", "gray.900")}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                >
+                    <IconButton
+                        size={"md"}
+                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                        aria-label={"Open Menu"}
+                        display={{ md: "none" }}
+                        onClick={isOpen ? onClose : onOpen}
+                    />
+                    <HStack spacing={8} alignItems={"center"}>
+                        <Flex alignItems="center">
+                            <Link href="/">
+                                <Image src="./klogo.jpg" width="80px" />
+                            </Link>
+                        </Flex>
+                        <HStack
+                            as={"nav"}
+                            spacing={4}
+                            display={{ base: "none", md: "flex" }}
+                        >
+                            <Link href="/">KSF</Link>
+                            <Link href="/">Projects</Link>
+                            <Link href="/">Skills</Link>
+
+                        </HStack>
+                    </HStack>
+                </Flex>
+            </Box>
+            <Box mx="auto" flex={1} p={4} maxW={"7xl"} width="100%">
+                {children}
+            </Box>
+            <Footer />
+        </Flex>
+    );
+};
+
+export default MainLayout;
