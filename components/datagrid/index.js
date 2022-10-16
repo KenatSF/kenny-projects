245f6a9f6990703aeeb014b7e78f84539c50de9c
KenatSF <banerabh@gmail.com>
2022-10-16 00:48:17 -0500

    Changes not effective

--- components/datagrid/index.js
+++ components/datagrid/index.js
@@ -1,4 +1,4 @@
-import { Flex, Text, Grid, Box } from "@chakra-ui/react";
+import { Flex, Text, Grid, Box, SimpleGrid, TableContainer, Table, Tbody, Tr, Td } from "@chakra-ui/react";
 
 import Projectinfo from "../projectinfo";
 
@@ -8,7 +8,7 @@ export default function Datagrid() {
     return (
         <Flex direction="column" justifyContent="center" alignItems="center" width="50vw" height="50vh" bgColor="white">
             <Box>
-                <Grid templateColumns="repeat(4, minmax(250px, 1fr))" gap={6}>
+                <Grid templateColumns="repeat(1, minmax(250px, 1fr))" gap={6}>
                     <Projectinfo titulillo={"Electricity consumption"} image={"./datascience/electricity.jpg"} linkTo={"electricity"} samePage={true}/>
                     <Projectinfo titulillo={"Stock Indices"} image={"./datascience/stockindices.jpg"} linkTo={"https://github.com/KenatSF/Heavy-Indexes"} samePage={false}/>
                     <Projectinfo titulillo={"Market sales"} image={"./datascience/market.jpg"} linkTo={"https://github.com/KenatSF/Basic_Data_Analysis"} samePage={false}/>
