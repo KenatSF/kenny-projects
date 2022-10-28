95ce545385deb33b5127fdf488b3ae3b7acb5bb5
KenatSF <banerabh@gmail.com>
2022-10-27 23:40:49 -0500

    Skills changes, visualization through a menu

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,4 @@
-import { Flex, Text, Box, TableContainer, Table, TableCaption, Thead, Tbody, Tr, Td, Th, Tfoot, TabPanels, Tab, TabList, Tabs, TabPanel, Link } from "@chakra-ui/react";
+import { Flex, Text, Box, TabPanels, Tab, TabList, Tabs, TabPanel, Link, Center, Image } from "@chakra-ui/react";
 import React from "react";
 
 import Datagrid from "../components/datagrid";
@@ -8,20 +8,29 @@ export default function Projects() {
 
     return (
         <Box flex="1" px={{ base: '25', md: '100', lg: '400' }} py="20">
-                <Tabs size="lg" colorScheme="white" align={{ base: 'end', md: 'center', lg: 'center' }} variant="enclosed">
-                    <TabList>
-                        <Tab fontWeight="bold">Data science</Tab>
-                        <Tab fontWeight="bold">Web 3.0</Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <Datagrid />
-                        </TabPanel>
-                        <TabPanel>
-                            <Web3grid />
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
+            <Center>
+                <Text fontSize={{ base: '24px', md: '30px', lg: '45px' }}  >Projects</Text>
+            </Center>
+            <Center>
+                <Box boxSize='xs'>
+                    <Image src="./portfolio.png" alt='projects' />
+                </Box>
+            </Center>
+            <br />
+            <Tabs size="lg" colorScheme="white" align={{ base: 'end', md: 'center', lg: 'center' }} variant="enclosed">
+                <TabList>
+                    <Tab fontWeight="bold">Data science</Tab>
+                    <Tab fontWeight="bold">Web 3.0</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <Datagrid />
+                    </TabPanel>
+                    <TabPanel>
+                        <Web3grid />
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
         </Box>
 
     )
--- pages/skills.js
+++ pages/skills.js
@@ -1,9 +1,9 @@
-import { Flex, Text, Box, TableContainer, Table, TableCaption, Thead, Tbody, Tr, Td, Th, Tfoot, Center, Image } from "@chakra-ui/react";
+import { Flex, Text, Box, Center, Image, Menu, Portal, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import React from "react";
 
 
 export default function Skills() {
-    
+
     return (
         <Box>
             <Center>
@@ -17,151 +17,188 @@ export default function Skills() {
             <br />
             <br />
             <br />
-            <Text fontSize='2xl' noOfLines={[1]} as='b'>
-                Data science Packages:
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Pandas
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Numpy
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Scikit-Learn
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Keras
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Statsmodels
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Networkx
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Nltk
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Matplotlib
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Seaborn
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - tseries
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - survival
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - ggplot2
-            </Text>
-            <br />
-            <Text fontSize='2xl' noOfLines={[1]} as='b'>
-                Programming Languages:
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Python.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - R.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Javascript.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Solidity.
-            </Text>
-            <br />
-            <Text fontSize='2xl' noOfLines={[1]} as='b'>
-                Database:
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - SQL.
-            </Text>
-            <br />
-            <Text fontSize='2xl' noOfLines={[1]} as='b'>
-                Data visualization
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Power BI.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Tableau.
-            </Text>
-            <br />
-            <Text fontSize='2xl' noOfLines={[1]} as='b'>
-                Frameworks:
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Next.js.
-            </Text>
-            <br />
-            <Text fontSize='2xl' noOfLines={[1]} as='b'>
-                Knowledge:
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Probability.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Statistics.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Generalized Linear Models.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Time Series.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Survival Analysis.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Network Theory.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Machine Learning.
-            </Text>
-            <br />
-            <Text fontSize='xl' noOfLines={[1, 2, 3]} >
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Deep Learning.
-            </Text>
+            <Menu>
+                <MenuButton>
+                    <Text fontSize={{ base: 'xl', md: '2xl', lg: '2xl' }}  noOfLines={[1]} as='b'>
+                        Data science Packages ↓
+                    </Text>
+                </MenuButton>
+                <Portal>
+                    <MenuList>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Pandas
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Numpy
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Scikit-Learn
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Keras
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Statsmodels
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Networkx
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Nltk
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Matplotlib
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Seaborn
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - tseries
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - survival
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - ggplot2
+                        </Text></MenuItem>
+                    </MenuList>
+                </Portal>
+            </Menu>
             <br />
             <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <Menu>
+                <MenuButton>
+                    <Text fontSize={{ base: 'xl', md: '2xl', lg: '2xl' }}  noOfLines={[1]} as='b'>
+                        Programming Languages ↓
+                    </Text>
+                </MenuButton>
+                <Portal>
+                    <MenuList>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Python.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - R.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Javascript.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Solidity.
+                        </Text></MenuItem>
 
+                    </MenuList>
+                </Portal>
+            </Menu>
+            <br />
+            <br />
+            <Menu>
+                <MenuButton>
+                    <Text fontSize={{ base: 'xl', md: '2xl', lg: '2xl' }}  noOfLines={[1]} as='b'>
+                        Database ↓
+                    </Text>
+                </MenuButton>
+                <Portal>
+                    <MenuList>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - SQL.
+                        </Text></MenuItem>
 
+                    </MenuList>
+                </Portal>
+            </Menu>
+            <br />
+            <br />
+            <Menu>
+                <MenuButton>
+                    <Text fontSize={{ base: 'xl', md: '2xl', lg: '2xl' }}  noOfLines={[1]} as='b'>
+                        Data visualization ↓
+                    </Text>
+                </MenuButton>
+                <Portal>
+                    <MenuList>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Power BI.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Tableau.
+                        </Text></MenuItem>
+
+                    </MenuList>
+                </Portal>
+            </Menu>
+            <br />
+            <br />
+            <Menu>
+                <MenuButton>
+                    <Text fontSize={{ base: 'xl', md: '2xl', lg: '2xl' }}  noOfLines={[1]} as='b'>
+                        Frameworks ↓
+                    </Text>
+                </MenuButton>
+                <Portal>
+                    <MenuList>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Next.js.
+                        </Text></MenuItem>
+
+                    </MenuList>
+                </Portal>
+            </Menu>
+            <br />
+            <br />
+            <Menu>
+                <MenuButton>
+                    <Text fontSize={{ base: 'xl', md: '2xl', lg: '2xl' }}  noOfLines={[1]} as='b'>
+                        Knowledge ↓
+                    </Text>
+                </MenuButton>
+                <Portal>
+                    <MenuList>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Probability.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Numpy
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Statistics.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Generalized Linear Models.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Time Series.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Survival Analysis.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Network Theory.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Machine Learning.
+                        </Text></MenuItem>
+                        <MenuItem><Text fontSize='xl' noOfLines={[1, 2, 3]} >
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Deep Learning.
+                        </Text></MenuItem>
+
+                    </MenuList>
+                </Portal>
+            </Menu>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
         </Box>
     )
 }
