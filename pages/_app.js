0e4076e15a74d5c451d85ccb7ad466ad3e2369ff
KenatSF <banerabh@gmail.com>
2022-10-30 23:34:45 -0600

    Audiences packaged added

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 
 import { ChakraProvider } from "@chakra-ui/react"
 import { DarkModeSwitch } from '../components/nav-link'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <DarkModeSwitch>
         < Component {...pageProps} />
+        <Analytics />
       </DarkModeSwitch>
     </ChakraProvider>
 
