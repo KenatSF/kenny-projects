b7ef4c8b4b0505b5c0e8adbdffa2883acb5b746f
KenatSF <banerabh@gmail.com>
2023-03-06 18:22:50 -0600

    lang simplified

--- components/web3grid/index.js
+++ components/web3grid/index.js
@@ -8,10 +8,10 @@ export default function Web3grid({data}) {
     return (
         <Box>
             <Grid templateColumns="repeat(1, minmax(250px, 1fr))" gap={6}>
-                <Projectinfo image={"/web3/light.jpg"} linkTo={"https://github.com/KenatSF/Flash_Loans_V3"} samePage={false} imageTittle={data.projectsFlashTittle} imageDescription={data.projectsFlashDescription} imageView={data.projectsFlashView} imageButton={data.projectsButton}/>
-                <Projectinfo image={"/web3/hacker.jpg"} linkTo={"https://github.com/KenatSF/Hacking_DeFi_Protocols"} samePage={false} imageTittle={data.projectsHacksTittle} imageDescription={data.projectsHacksDescription} imageView={data.projectsHacksView} imageButton={data.projectsButton}/>
-                <Projectinfo image={"/web3/nft.jpg"} linkTo={"https://github.com/KenatSF/MarketCore/tree/main/contracts"} samePage={false} imageTittle={data.projectsNFTTittle} imageDescription={data.projectsNFTDescription} imageView={data.projectsNFTView} imageButton={data.projectsButton}/>
-                <Projectinfo image={"/web3/multisig.jpg"} linkTo={"https://github.com/KenatSF/Solidity_Multisig_Wallet/blob/main/contracts/Firmas.sol"} samePage={false} imageTittle={data.projectsMultiTittle} imageDescription={data.projectsMultiDescription} imageView={data.projectsMultiView} imageButton={data.projectsButton}/>
+                <Projectinfo image={"/web3/light.jpg"} linkTo={"https://github.com/KenatSF/Flash_Loans_V3"} samePage={false} imageTittle={data.projectsFlashTittle} imageDescription={data.projectsFlashDescription} imageView={data.code} imageButton={data.projectsButton}/>
+                <Projectinfo image={"/web3/hacker.jpg"} linkTo={"https://github.com/KenatSF/Hacking_DeFi_Protocols"} samePage={false} imageTittle={data.projectsHacksTittle} imageDescription={data.projectsHacksDescription} imageView={data.code} imageButton={data.projectsButton}/>
+                <Projectinfo image={"/web3/nft.jpg"} linkTo={"https://github.com/KenatSF/MarketCore/tree/main/contracts"} samePage={false} imageTittle={data.projectsNFTTittle} imageDescription={data.projectsNFTDescription} imageView={data.code} imageButton={data.projectsButton}/>
+                <Projectinfo image={"/web3/multisig.jpg"} linkTo={"https://github.com/KenatSF/Solidity_Multisig_Wallet/blob/main/contracts/Firmas.sol"} samePage={false} imageTittle={data.projectsMultiTittle} imageDescription={data.projectsMultiDescription} imageView={data.code} imageButton={data.projectsButton}/>
             </Grid>
         </Box>
 
